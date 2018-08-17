3da38904c00cebbe01a0002664b3693df295b6ff
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-17 07:19:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/' do
+  post '/display_puppy' do
     @display_puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
