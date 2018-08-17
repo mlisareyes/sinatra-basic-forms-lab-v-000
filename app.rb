a2e09e2d8b306b2afcf8d8b410faaa13198f0303
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-17 07:10:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
@@ -9,8 +11,9 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy' do
+  post '/' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
+
 end
